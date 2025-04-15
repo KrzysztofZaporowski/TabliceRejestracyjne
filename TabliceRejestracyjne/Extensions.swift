83f6cabd8b77c6791af388fcd41e7cc13e0350f3
KrzysztofZaporowski <krzysztofzaporowski@gmail.com>
2025-04-15 12:44:29 +0200

    FirstCommit

---
+++ TabliceRejestracyjne/Extensions.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+extension String {
+    subscript(_ range: CountableRange<Int>) -> String {
+        let start = index(startIndex, offsetBy: max(0, range.lowerBound))
+        let end = index(startIndex, offsetBy: min(self.count, range.upperBound))
+        return String(self[start..<end])
+    }
+
+    subscript(_ range: CountablePartialRangeFrom<Int>) -> String {
+        let start = index(startIndex, offsetBy: max(0, range.lowerBound))
+         return String(self[start...])
+    }
+}
