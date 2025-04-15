c5c8429eacbc26f781d06464af3b617dd4fd59b9
KrzysztofZaporowski <krzysztofzaporowski@gmail.com>
2025-04-15 12:10:07 +0200

    Initial Commit

---
+++ TabliceRejestracyjne/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TabliceRejestracyjne
+//
+//  Created by Krzysztof Zaporowski on 15/04/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TabliceRejestracyjneTests/TabliceRejestracyjneTests.swift
@@ -0,0 +1,17 @@
+//
+//  TabliceRejestracyjneTests.swift
+//  TabliceRejestracyjneTests
+//
+//  Created by Krzysztof Zaporowski on 15/04/2025.
+//
+
+import Testing
+@testable import TabliceRejestracyjne
+
+struct TabliceRejestracyjneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
